2d015b4408ba537ae2b6cfe7852c76430a9614c3
Dmitry Petrov <Dima.demon.net@mail.ru>
2024-02-09 20:17:24 +0400

    feat: Подготовить проект к написанию вёрстки

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.(woff|woff2|eot|ttf|otf)$/i.test(assetInfo.name)) {
+            return `fonts/${assetInfo.name}`;
+          }
+          return `assets/${assetInfo.name}`; // Оставляем остальные файлы в папке assets
+        },
+      },
+    },
+  },
+});
