cb4302617d903caddc54b7158e046c3a75a10536
Dmitry Petrov <Dima.demon.net@mail.ru>
2024-02-18 22:40:46 +0400

    style: Подправить стиль активных ссылок

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,20 +26,20 @@ function Header({ login }) {
           <>
             <nav className="header__container">
               <ul className='header__list header__list_auth'>
-                <li><Link className='header__link header__link_auth' to="/movies">Фильмы</Link></li>
-                <li><Link className='header__link header__link_auth' to="/saved-movies">Сохранённые фильмы</Link></li>
+                <li><Link className={`header__link header__link_auth ${currentPath === '/movies' ? 'header__link_active' : ''}`} to="/movies">Фильмы</Link></li>
+                <li><Link className={`header__link header__link_auth ${currentPath === '/saved-movies' ? 'header__link_active' : ''}`} to="/saved-movies">Сохранённые фильмы</Link></li>
               </ul>
               <Link className={currentPath === '/' ? 'header__link_img' : 'header__link_img header__link_img_dark'} to="/profile"></Link>
             </nav>
             <nav className={isBurgerMenuOpen ? "header__container_mobile header__container_mobile-open" : "header__container_mobile"}>
               <ul className='header__list header__list_auth-mobile'>
-                <li><Link className='header__link header__link_auth' to="/">Главная</Link></li>
-                <li><Link className='header__link header__link_auth' to="/movies">Фильмы</Link></li>
-                <li><Link className='header__link header__link_auth' to="/saved-movies">Сохранённые фильмы</Link></li>
+                <li><Link className={`header__link header__link_auth ${currentPath === '/' ? 'header__link_active' : ''}`} to="/">Главная</Link></li>
+                <li><Link className={`header__link header__link_auth ${currentPath === '/movies' ? 'header__link_active' : ''}`} to="/movies">Фильмы</Link></li>
+                <li><Link className={`header__link header__link_auth ${currentPath === '/saved-movies' ? 'header__link_active' : ''}`} to="/saved-movies">Сохранённые фильмы</Link></li>
               </ul>
               <Link className={currentPath === '/' ? 'header__link_img' : 'header__link_img header__link_img_dark'} to="/profile"></Link>
             </nav>
-            <button className={isBurgerMenuOpen ? "header__button_mobile-close rotateClockwise" : "header__button_mobile rotateCounterClockwise"} onClick={handleBurgerClick}></button>
+            <button className={isBurgerMenuOpen ? "header__button_mobile-close rotateClockwise" : "header__button_mobile "} onClick={handleBurgerClick}></button>
           </>
         ) : (
           <ul className='header__list'>
